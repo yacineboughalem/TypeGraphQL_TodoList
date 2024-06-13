5272ef0f847a9a0589b65c7bb101a84fa96db4d0
yacineboughalem <yacin.boughalem@gmail.com>
2024-06-13 17:26:03 +0100

    Update / Delete Task

--- src/resolvers/task.ts
+++ src/resolvers/task.ts
@@ -8,6 +8,26 @@ export class TaskResolver {
     return "hello frontYnova";
   }
 
+  @Query(() => [Task])
+  tasks(): Promise<Task[]> {
+    return Task.find({});
+  }
+
+
+  // @Query(() => Task, { nullable: true })
+  // task(
+  //   @Arg("id", () => Int)
+  //   id: number
+  // ): Promise<Task | undefined> {
+  //   return Task.findOne({ id })
+  // }
+
+  @Query(() => Task, { nullable: true })
+  async task(
+    @Arg("id", () => Int) id: number
+  ): Promise<Task | undefined> {
+    return await Task.findOne({ where: { id } });
+  }
 
   @Mutation(() => Task)
   createTask(
@@ -16,4 +36,35 @@ export class TaskResolver {
   ): Promise<Task> {
     return Task.create({ title, isComplete: false }).save();
   }
+
+  @Mutation(() => Boolean)
+  deleteTask(
+    @Arg("id", () => Int)
+    id: number
+  ): boolean {
+    try {
+      Task.delete({ id });
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+
+  @Mutation(() => Boolean, { nullable: true })
+  updateTask(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String) title: string,
+    @Arg("isComplete", () => Boolean) isComplete: boolean
+
+  ): boolean | null {
+    const task = Task.findOne({ where: { id } })
+    if (!task) return null;
+
+    try {
+      Task.update({ id }, { title, isComplete })
+      return true;
+    } catch { return false; }
+  }
+
 }
